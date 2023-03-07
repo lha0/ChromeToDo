57591f994d4a9bba7a60a048b80066d91705fc27
lha0 <lha0@unist.ac.kr>
2023-03-07 22:09:43 +0900

    gitignore 적용

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,7 @@
-const API_KEYS = "7a73a3a1760c3a7ed27e6261c5643483";
-
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEYS}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${globalVariable.API_KEYS}&units=metric`;
     fetch(url)
         .then(response => response.json())
         .then(data => {
